970530e7769129c8824ace4dfec2022c3e663c74
mat-teacher <beyefendi.023@gmail.com>
2024-09-14 02:26:38 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSubjects(className) {
+    // Sınıf seçildiğinde yapılacak işlemleri buraya ekleyebilirsiniz.
+    // Örneğin, konuların gösterilmesi veya belirli bir sınıfa göre değişiklik yapılması gibi.
+    console.log('Seçilen sınıf:', className);
+    // Bu örnekte herhangi bir özel işlev eklemiyoruz; ancak burayı özelleştirebilirsiniz.
+}
